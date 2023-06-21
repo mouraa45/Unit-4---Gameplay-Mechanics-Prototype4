f9e92f0006f86fe18031fc87d5447b19275b5b7d
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 21:38:21 +0100

    varuios power ups configurados

--- Assets/scripts/PowerUp.cs
+++ Assets/scripts/PowerUp.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum PowerUpType { None, Pushback, Rockets }
+public enum PowerUpType { None, Pushback, Rockets,Smash }
 
 public class PowerUp : MonoBehaviour
 {
