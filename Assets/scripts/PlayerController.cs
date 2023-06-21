27e9a50b6face7d8849d8012ed6f01d92ac6bdc4
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 11:21:30 +0100

    bouncy no player e ini

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint=GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
