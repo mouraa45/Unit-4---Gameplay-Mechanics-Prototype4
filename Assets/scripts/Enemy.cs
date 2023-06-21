8bd76467eb6e385abd4c8d9f848f4b7437fe5cbb
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 22:22:39 +0100

    tarefas de bonus terminadas
    terminado

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -7,34 +7,41 @@ public class Enemy : MonoBehaviour
 	private Rigidbody enemyRB;
 	private GameObject player;
 	public float speed;
+	public bool isBoss = false;
+	public float spawnInterval;
+	private float nextSpawn;
+	public int miniEnemySpawnCount;
+	private SpawnManager spawnManager;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		enemyRB = GetComponent<Rigidbody>();
 		player = GameObject.Find("Player");
-		
-		
-		
-		
+		if (isBoss)
+		{
+			spawnManager = FindObjectOfType<SpawnManager>();
+		}
 	}
-	
-	
-	
+
 	// Update is called once per frame
 	void Update()
 	{
-		Vector3 lookDirection =(player.transform.position - transform.position).normalized;
+		Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 		enemyRB.AddForce(lookDirection * speed);
-		if(transform.position.y<-10)
+
+		if (isBoss)
 		{
-			Destroy (gameObject);
-		
-		
+			if (Time.time > nextSpawn)
+			{
+				nextSpawn = Time.time + spawnInterval;
+				spawnManager.SpawnMiniEnemy(miniEnemySpawnCount);
+			}
 		}
-			
+
+		if (transform.position.y < -10)
+		{
+			Destroy(gameObject);
+		}
 	}
-	
-	
-	
-	
 }
--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -4,49 +4,85 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] enemyPrefabs;
-    public int enemyCount;
-    private float spawnRange = 9;
-    public int waveNumber = 1;
-    public GameObject[] powerupPrefabs;
+	public GameObject[] enemyPrefabs;
+	public int enemyCount;
+	private float spawnRange = 9;
+	public int waveNumber = 1;
+	public GameObject[] powerupPrefabs;
+	public GameObject bossPrefab;
+	public GameObject[] miniEnemyPrefabs;
+	public int bossRound=5;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
+	{
+		int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+		Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(),
+		            powerupPrefabs[randomPowerup].transform.rotation);
+		SpawnEnemyWave(waveNumber);
+	}
+
+	private Vector3 GenerateSpawnPosition()
+	{
+		float spawnPosX = Random.Range(-spawnRange, spawnRange);
+		float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+		Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+		return randomPos;
+	}
+	
+	void SpawnBossWave(int currentRound)
+{
+    int miniEnemiesToSpawn;
+    if (bossRound != 0)
     {
+        miniEnemiesToSpawn = currentRound / bossRound;
+    }
+    else
+    {
+        miniEnemiesToSpawn = 1;
+    }
+
+    var boss = Instantiate(bossPrefab, GenerateSpawnPosition(), bossPrefab.transform.rotation);
+    boss.GetComponent<Enemy>().miniEnemySpawnCount = miniEnemiesToSpawn;
+}
+	public void SpawnMiniEnemy(int amount)
+	{
+		for(int i = 0; i < amount; i++)
+		{
+			int randomMini = Random.Range(0, miniEnemyPrefabs.Length);
+			Instantiate(miniEnemyPrefabs[randomMini], GenerateSpawnPosition(),
+			            miniEnemyPrefabs[randomMini].transform.rotation);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+{
+    enemyCount = FindObjectsOfType<Enemy>().Length;
+    if (enemyCount == 0)
+    {
+        waveNumber++;
+        if (bossRound != 0 && waveNumber % bossRound == 0)
+        {
+            SpawnBossWave(waveNumber);
+        }
+        else
+        {
+            SpawnEnemyWave(waveNumber);
+        }
+
         int randomPowerup = Random.Range(0, powerupPrefabs.Length);
         Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(),
-            powerupPrefabs[randomPowerup].transform.rotation);
-        SpawnEnemyWave(waveNumber);
+                    powerupPrefabs[randomPowerup].transform.rotation);
     }
+}
 
-    private Vector3 GenerateSpawnPosition()
-    {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        return randomPos;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        enemyCount = FindObjectsOfType<Enemy>().Length;
-        if (enemyCount == 0)
-        {
-            waveNumber++;
-            SpawnEnemyWave(waveNumber);
-            int randomPowerup = Random.Range(0, powerupPrefabs.Length);
-            Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(),
-                powerupPrefabs[randomPowerup].transform.rotation);
-        }
-    }
-
-    void SpawnEnemyWave(int enemiesToSpawn)
-    {
-        for (int i = 0; i < enemiesToSpawn; i++)
-        {
-            int randomEnemy = Random.Range(0, enemyPrefabs.Length);
-            Instantiate(enemyPrefabs[randomEnemy], GenerateSpawnPosition(), enemyPrefabs[randomEnemy].transform.rotation);
-        }
-    }
+	void SpawnEnemyWave(int enemiesToSpawn)
+	{
+		for (int i = 0; i < enemiesToSpawn; i++)
+		{
+			int randomEnemy = Random.Range(0, enemyPrefabs.Length);
+			Instantiate(enemyPrefabs[randomEnemy], GenerateSpawnPosition(), enemyPrefabs[randomEnemy].transform.rotation);
+		}
+	}
 }
